2a8fe3c613791683ecb00f5d20eb392213684ccb
Ryan Perera <ryanperera9001@gmail.com>
2019-07-30 22:30:16 -0400

    Implemented external library for fancy toast messages

--- app/src/main/java/com/example/socialmediaapp/SignUp.java
+++ app/src/main/java/com/example/socialmediaapp/SignUp.java
@@ -2,7 +2,8 @@ package com.example.socialmediaapp;
 
 import android.os.Bundle;
 import android.view.View;
-import android.widget.Toast;
+import android.widget.Button;
+import android.widget.EditText;
 
 import androidx.appcompat.app.AppCompatActivity;
 
@@ -10,29 +11,59 @@ import com.parse.ParseException;
 import com.parse.ParseInstallation;
 import com.parse.ParseObject;
 import com.parse.SaveCallback;
+import com.shashank.sony.fancytoastlib.FancyToast;
 
-public class SignUp extends AppCompatActivity {
+public class SignUp extends AppCompatActivity  implements View.OnClickListener{
+
+    private Button btnSave;
+    private EditText editName, editPunchSpeed, editPunchPower, editKickSpeed, editKickPower;
+
 
     @Override
-    protected void onCreate(Bundle savedInstanceState) {
+    protected void onCreate(Bundle savedInstanceState)    {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_sign_up);
 
         // Save the current Installation to Back4App
         ParseInstallation.getCurrentInstallation().saveInBackground();
+
+        btnSave = findViewById(R.id.btnSave);
+
+        btnSave.setOnClickListener(SignUp.this);
+
+        editName = findViewById(R.id.editName);
+        editPunchSpeed = findViewById(R.id.editPunchSpeed);
+        editPunchPower = findViewById(R.id.editPunchPower);
+        editKickSpeed = findViewById(R.id.editKickSpeed);
+        editKickPower = findViewById(R.id.editKickPower);
+
+
+
     }
 
-    public void helloWorldTapped(View view){
+    @Override
+    public void onClick(View view) {
 
-        ParseObject boxer = new ParseObject("Boxer");
-        boxer.put("Punch_Speed", 200);
-        boxer.saveInBackground(new SaveCallback() {
-            @Override
-            public void done(ParseException e) {
-                if (e==null){
-                    Toast.makeText(SignUp.this,"Object saved successfully", Toast.LENGTH_LONG).show();
+        try {
+            final ParseObject kickBoxer = new ParseObject("kickBoxer");
+            kickBoxer.put("name", editName.getText().toString());
+            kickBoxer.put("punchSpeed", Integer.parseInt(editPunchSpeed.getText().toString()));
+            kickBoxer.put("punchPower", Integer.parseInt(editPunchPower.getText().toString()));
+            kickBoxer.put("kickSpeed", Integer.parseInt(editKickSpeed.getText().toString()));
+            kickBoxer.put("kickPower", Integer.parseInt(editKickPower.getText().toString()));
+            kickBoxer.saveInBackground(new SaveCallback() {
+                @Override
+                public void done(ParseException e) {
+                    if (e == null) {
+                        FancyToast.makeText(SignUp.this, kickBoxer.get("name") + " sent to server", FancyToast.LENGTH_LONG, FancyToast.SUCCESS, false).show();
+                    } else {
+                        FancyToast.makeText(SignUp.this, e.getMessage(), FancyToast.LENGTH_LONG, FancyToast.ERROR, false).show();
+                    }
                 }
-            }
-        });
+            });
+        }
+        catch (Exception e){
+            FancyToast.makeText(SignUp.this, e.getMessage(), FancyToast.LENGTH_LONG, FancyToast.ERROR, false).show();
+        }
     }
 }
