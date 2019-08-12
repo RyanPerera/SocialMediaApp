2d205011f976621600a991e2be5682c03b575812
Ryan Perera <ryanperera9001@gmail.com>
2019-08-12 19:30:21 -0400

    Can now see list of other users in Users tab

--- app/src/main/java/com/example/socialmediaapp/ProfileTab.java
+++ app/src/main/java/com/example/socialmediaapp/ProfileTab.java
@@ -5,14 +5,24 @@ import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.EditText;
 
 import androidx.fragment.app.Fragment;
+
+import com.parse.ParseException;
+import com.parse.ParseUser;
+import com.parse.SaveCallback;
+import com.shashank.sony.fancytoastlib.FancyToast;
 
 
 /**
  * A simple {@link Fragment} subclass.
  */
 public class ProfileTab extends Fragment {
+
+    private EditText edtDisplayName, edtBio;
+    private Button btnUpdateInfo;
 
 
     public ProfileTab() {
@@ -24,7 +34,47 @@ public class ProfileTab extends Fragment {
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_profile_tab, container, false);
+        View view = inflater.inflate(R.layout.fragment_profile_tab, container, false);
+
+        edtDisplayName = view.findViewById(R.id.edtDisplayName);
+        edtBio = view.findViewById(R.id.edtBio);
+
+        btnUpdateInfo = view.findViewById(R.id.btnUpdateInfo);
+
+        final ParseUser parseUser =  ParseUser.getCurrentUser();
+
+        // Show previously inputted User information
+        if (parseUser.get("displayName") == null){
+            edtDisplayName.setText("");
+        } else {
+            edtDisplayName.setText(parseUser.get("displayName").toString());
+        }
+        if (parseUser.get("bio") == null){
+            edtBio.setText("");
+        } else {
+            edtBio.setText(parseUser.get("bio").toString());
+        }
+
+        // Update User information
+        btnUpdateInfo.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                parseUser.put("displayName", edtDisplayName.getText().toString());
+                parseUser.put("bio", edtBio.getText().toString());
+                parseUser.saveInBackground(new SaveCallback() {
+                    @Override
+                    public void done(ParseException e) {
+                        if (e==null) {
+                            FancyToast.makeText(getContext(), "Profile updated successfully!", FancyToast.LENGTH_LONG, FancyToast.SUCCESS, false).show();
+                        } else{
+                            FancyToast.makeText(getContext(), e.getMessage(), FancyToast.LENGTH_SHORT, FancyToast.ERROR, false).show();
+
+                        }
+                    }
+                });
+                }
+        });
+        return view;
     }
 
 }
--- app/src/main/java/com/example/socialmediaapp/UsersTab.java
+++ app/src/main/java/com/example/socialmediaapp/UsersTab.java
@@ -5,8 +5,19 @@ import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
+import android.widget.TextView;
 
 import androidx.fragment.app.Fragment;
+
+import com.parse.FindCallback;
+import com.parse.ParseException;
+import com.parse.ParseQuery;
+import com.parse.ParseUser;
+
+import java.util.ArrayList;
+import java.util.List;
 
 
 /**
@@ -14,6 +25,9 @@ import androidx.fragment.app.Fragment;
  */
 public class UsersTab extends Fragment {
 
+    private ListView listView;
+    private ArrayList arrayList;
+    private ArrayAdapter arrayAdapter;
 
     public UsersTab() {
         // Required empty public constructor
@@ -24,7 +38,34 @@ public class UsersTab extends Fragment {
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_users_tab, container, false);
+        View view = inflater.inflate(R.layout.fragment_users_tab, container, false);
+
+        listView = view.findViewById(R.id.listView);
+        arrayList = new ArrayList();
+        arrayAdapter = new ArrayAdapter(getContext(), android.R.layout.simple_list_item_1, arrayList);
+
+        final TextView txtLoadingUsers = view.findViewById(R.id.txtLoadingUsers);
+
+        ParseQuery<ParseUser> parseQuery = ParseUser.getQuery();
+        parseQuery.whereNotEqualTo("displayName", ParseUser.getCurrentUser().getUsername());
+        parseQuery.findInBackground(new FindCallback<ParseUser>() {
+            @Override
+            public void done(List<ParseUser> objects, ParseException e) {
+                if (e==null){
+                    if (objects.size() > 0){
+                        for (ParseUser user : objects){
+                            arrayList.add(user.getUsername());
+                        }
+
+                        listView.setAdapter(arrayAdapter);
+                        txtLoadingUsers.setVisibility(View.INVISIBLE);
+                        listView.setVisibility(View.VISIBLE);
+                    }
+                }
+            }
+        });
+
+        return view;
     }
 
 }
